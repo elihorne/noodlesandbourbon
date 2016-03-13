43b159e3337f0a8c647678ffa02b6505175f88db
Eli Horne <elihorne@gmail.com>
2016-03-13 12:38:35 -0400

    placeholders

---
+++ function.js
@@ -0,0 +1,28 @@
+function slider(activeSlider){
+  activeSlider.find('.content:first-child').show();
+  activeSlider.find('.slider-nav a').on('click', function(event){
+    event.preventDefault();
+    activeSlider.find('.slider-nav a').removeClass('active');
+    var targetSliderSection = '.' + $(this).attr('class');
+    console.log(targetSliderSection);
+    $(this).addClass('active');
+
+    activeSlider.find('.content').hide();
+    activeSlider.find(targetSliderSection).fadeIn();
+  });
+}
+
+$('.slider').each(function(){
+  var activeSlider = $(this);
+  slider(activeSlider);
+});
+
+function stickyNav() {
+  $('body').append('<nav class="sticky-nav"></nav>');
+  $('.main-nav a').each(function(){
+    var clonedItem = $(this).clone();
+    $('.sticky-nav').append(clonedItem);
+  });
+}
+
+stickyNav();
